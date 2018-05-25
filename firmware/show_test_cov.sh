0f02b56456d0420379b61d89534e3ed4dca8316f
Emily Björk <EmilyBjoerk@users.noreply.github.com>
2018-05-25 22:34:17 +0200

    Adding script to easily show coverage information of tests.

--- firmware/show_test_cov.sh
+++ firmware/show_test_cov.sh
@@ -3,4 +3,10 @@ rm default.profraw
 rm testcov.profdata
 make test
 llvm-profdata merge -o testcov.profdata default.profraw
-llvm-cov show ./native_tests -instr-profile=testcov.profdata "$@"
+
+if [ $# -eq 0 ]; then
+    llvm-cov report -instr-profile=testcov.profdata ./native_tests
+else
+    llvm-cov show ./native_tests -instr-profile=testcov.profdata "$@"
+fi
+
